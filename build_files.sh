7f3b0954081556e1c22c7dc09c39ff0d8bc55940
kidusMAT <kidusmezgebe2@gmail.com>
2026-02-25 20:14:37 +0300

    Fix python version in build script

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Ensure we use python3.11 for the build
-export PYTHON_VERSION=3.11
-
 echo "Installing dependencies..."
-python3.11 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Collecting static files..."
 # Make sure the directory exists before collecting
 mkdir -p staticfiles
-python3.11 manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input
 
 echo "Build complete!"
